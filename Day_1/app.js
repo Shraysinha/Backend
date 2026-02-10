70ce0fc4afd74ae1ab7e45e0a86c7445639acd72
“shray <shraypriyadarshi@gmail.com>
2026-02-10 12:42:18 +0530

    Add Day 1 backend files

---
+++ Day_1/app.js
@@ -0,0 +1,23 @@
+// Here we learn how to use packages that we downloaded . For exapmle:_this Terminal code
+// shraypriyadarshi@Shrays-MacBook-Air Day_1 % npm i cat-me
+
+// added 17 packages, and audited 83 packages in 2s
+
+// 22 packages are looking for funding
+//   run `npm fund` for details
+
+// found 0 vulnerabilities
+// shraypriyadarshi@Shrays-MacBook-Air Day_1 % node app.js
+//   /\ ___ /\
+//  (  o   o  )
+//   \  >#<  /
+//   /       \
+//  /         \       ^
+// |           |     //
+//  \         /    //
+//   ///  ///   --
+
+
+// Code of app.js
+const catMe = require("cat-me")
+console.log(catMe())
