20fd11ff9e85eab9354190e3dc15ba77b81dcef3
“shray <shraypriyadarshi@gmail.com>
2026-02-22 10:48:02 +0530

    Frontend Integration complete

--- Day_9/Frontend/src/App.jsx
+++ Day_9/Frontend/src/App.jsx
@@ -1,25 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
+// import { estimatedDocumentCount } from '../../Backend/src/models/note.model'
 
 function App() {
-  const [notes, setNotes] = useState([
-    
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get('http://localhost:3000/api/notes')
+
+  function fetchNotes(){
+    axios.get('http://localhost:3000/api/notes')
   .then((res)=>{
     setNotes(res.data.notes)
   })
+  }
+  useEffect(()=>{
+    fetchNotes()
+  }, [])
+  
+  function handleSubmit(e){
+    e.preventDefault()
+    const {title , description} = e.target.elements
+    console.log(title.value, description.value)
+    axios.post("http://localhost:3000/api/notes",{
+      title: title.value,
+      description: description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+  }
 
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+    console.log(noteId)
+  }
 
   return (
     <>
+
+    <form className="note-create-form" onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='Enter Title'/>
+      <input name='description' type="text" placeholder='Enter description'/>
+      <button>Create Note</button>
+    </form>
+
+
     <div className="notes">
       {
         notes.map(note=>{
         return  <div className="note">
             <h1>{note.title}</h1>
             <p>{note.description}</p>
+            <button onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
            </div>
         })
       }
