0ce50db9d9205251183393205084a479bb02ba64
“shray <shraypriyadarshi@gmail.com>
2026-05-13 00:35:10 +0530

    Bcyptjs completed

---
+++ Bcyptjs/src/routes/auth.routes.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const authRouter = express.Router()
+const crypto = require("crypto")
+
+
+authRouter.post("/register", async(req,res)=>{
+  const { email, name, password } = req.body
+  const isUserAlreadyExists = await userModel.findOne({ email })
+
+  if(isUserAlreadyExists){
+    return res.status(400).json({
+      message : "With this Email User Already Exists"
+    })
+  }
+
+  const hash = crypto.createHash("md5").update(password).digest("hex")
+
+  const user = await userModel.create({
+    email ,password: hash,name
+  })
+
+  const token = jwt.sign(
+    {
+    id : user._id,
+    email : user.email
+    },
+    process.env.JWT_SECRET
+)
+
+res.cookie("jwt_token",token)
+
+  res.status(201).json({
+    message : "User Registered Succesfully",
+    user,
+    token
+  })
+
+})
+
+authRouter.post("/login", async(req,res)=>{
+  const {email , password } = req.body
+  const user = await userModel.findOne({email})
+
+  if(!user){
+    return res.status(404).json({
+      message : "User not found with this Email "
+    })
+  }
+  const isPasswordMathched = user.password === crypto.createHash("md5").update(password).digest("hex")
+
+  if(!isPasswordMathched){
+    return res.status(401).json({
+      message : "Invalid password"
+    })
+  }
+
+  const token = jwt.sign({
+    id: user._id,
+  }, process.env.JWT_SECRET)
+
+  res.cookie("jwt_token", token)
+
+  res.status(200).json({
+    message: "User loged in",
+    user
+  })
+
+})
+
+module.exports = authRouter
