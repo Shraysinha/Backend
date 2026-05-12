34f3c3593eb42672d75160b9a81ec88dc4b32241
“shray <shraypriyadarshi@gmail.com>
2026-05-12 23:21:28 +0530

    revision add

---
+++ Day-12/src/config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+
+function connectToDB(){
+  mongoose.connect(process.env.MONGO_URI)
+  .then(()=>{
+    console.log("Connected to DB")
+  })
+}
+
+module.exports = connectToDB
---
+++ Day-12/src/routes/auth.routes.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+
+const authRouter = express.Router()
+
+
+authRouter.post("/register", async(req,res)=>{
+  const { email, name, password } = req.body
+  const isUserAlreadyExists = await userModel.findOne({ email })
+
+  if(isUserAlreadyExists){
+    return res.status(400).json({
+      message : "With this Email User Already Exists"
+    })
+  }
+
+
+  const user = await userModel.create({
+    email ,password ,name
+  })
+
+  const token = jwt.sign(
+    {
+    id : user._id,
+    email : user.email
+    },
+    process.env.JWT_SECRET
+)
+
+res.cookie("jwt_token",token)
+
+  res.status(201).json({
+    message : "User Registered Succesfully",
+    user,
+    token
+  })
+
+})
+
+module.exports = authRouter
